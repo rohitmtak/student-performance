b72844fb727f8b3875262b27168ed58a2a8ecd0b
Rohit <rohitmtak@gmail.com>
2024-04-28 01:15:01 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,5 @@
-import os
+#import necessary modules
+import os  # import the os module for operating system functionality
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -6,15 +7,14 @@ import pandas as pd
 from src.utils import read_sql_data
 
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifacts','train.csv')
-    test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts','raw.csv')
+    train_data_path:str=os.path.join('artifacts', 'train.csv')
+    test_data_path:str=os.path.join('artifacts', 'test.csv')
+    raw_data_path:str=os.path.join('artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -22,24 +22,28 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
-            # reading the data from mysql
-            df=read_sql_data()
+            # read raw data from MySql
+            df=pd.read_csv(os.path.join('notebook/data', 'raw.csv'))
             logging.info("Reading completed mysql database")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-
-            df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True)
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            # save raw data to a file
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            # split data into train and test sets
+            train_set,test_set=train_test_split(df,test_size=0.2, random_state=42)
+            
+            # save train and test sets to a separate files
             train_set.to_csv(self.ingestion_config.train_data_path,index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
 
-            logging.info("Data Ingestion is completed")
+            logging.info("Data ingestion is completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
             
         except Exception as e:
+            logging.info("Exception occured while ingesting data")
             raise CustomException(e,sys)
