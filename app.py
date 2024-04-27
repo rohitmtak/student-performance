e73c1b0229c9f77d5357a879665fba2f6045da81
Rohit <rohitmtak@gmail.com>
2024-04-27 09:50:31 +0530

    Data Ingestion and Transformation

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+# import necessary modules
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
 from src.components.data_ingestion import DataIngestionConfig
-import sys
+from src.components.data_transformation import DataTransformationConfig, DataTransformation
 
+import sys # import the sys module for system-specific parameters and functions
 
 if __name__=="__main__":
     logging.info("Execution started")
     
     try:
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        # initiate data ingestion process and get paths for training and test data
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         
+        data_transformation = DataTransformation()
+        # initiate data transformation process
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path) 
         
+                
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
