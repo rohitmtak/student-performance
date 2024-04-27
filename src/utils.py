f149730128ded7d92a06523547f25a487c907a0f
Rohit <rohitmtak@gmail.com>
2024-04-28 01:56:52 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql # for MySQL database connection
 
 import pickle
@@ -50,3 +52,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {} # dictionary to store model evaluation scores
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model_params=param[list(models.keys())[i]]
+            
+            # perform grid search to find the best hyperparameters using cross-validation
+            gs = GridSearchCV(model, model_params, cv=3)
+            gs.fit(X_train, y_train)
+            
+            # set the model's parameters to the best found and train the model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
